a93ccd1d41b517d35e886b853a1f6f022f6d4092
Rachit <singhrachit569@gmail.com>
2022-05-02 12:09:14 +0530

    final code

--- README.md
+++
@@ -1,1 +0,0 @@
-# uddipan-project
---
+++ uddipan-project/config.py
@@ -0,0 +1,9 @@
+config = {}
+config["LogFolder"] = "logs"
+
+config['MYSQL_HOST'] = '34.100.188.132'  # hostname
+config['MYSQL_USER'] = 'root'  # username
+config['MYSQL_PASSWORD'] = 'password'  # password
+config['MYSQL_DB'] = 'uddipan'  # database name
+
+config["MaxNInstance"] = 1
