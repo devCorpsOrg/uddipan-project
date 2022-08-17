b8c776532b2f599fd348a69a688c4ae4b825ebd6
rachit0904 <singhrachit569@gmail.com>
2022-08-17 19:58:46 +0530

    needed

--- uddipan-project/config.py
+++
@@ -1,9 +0,0 @@
-config = {}
-config["LogFolder"] = "logs"
-
-config['MYSQL_HOST'] = '34.100.188.132'  # hostname
-config['MYSQL_USER'] = 'root'  # username
-config['MYSQL_PASSWORD'] = 'password'  # password
-config['MYSQL_DB'] = 'uddipan'  # database name
-
-config["MaxNInstance"] = 1
