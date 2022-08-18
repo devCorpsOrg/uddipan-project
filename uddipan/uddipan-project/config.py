7a605acf033cfa81f0b4adc7f46841f4504b3b73
rachit0904 <singhrachit569@gmail.com>
2022-08-18 22:10:06 +0530

    initial phase

--- uddipan/uddipan-project/config.py
+++
@@ -1,9 +0,0 @@
-config = {}
-config["LogFolder"] = "logs"
-
-config['MYSQL_HOST'] = '128.199.122.126'  # hostname
-config['MYSQL_USER'] = 'averps'  # username
-config['MYSQL_PASSWORD'] = 'Dev@averps3985'  # password
-config['MYSQL_DB'] = 'uddipan'  # database name
-
-config["MaxNInstance"] = 1
