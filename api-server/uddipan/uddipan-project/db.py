7cbab431ec1dd90c4a9e579ba5a159996ba15ec2
ParthBarse <parthbarse72@gmail.com>
2022-08-20 01:31:25 +0530

    update

--- api-server/uddipan/uddipan-project/db.py
+++ api-server/uddipan/uddipan-project/db.py
@@ -10,6 +10,26 @@ from config import *
 
 import traceback
 
+def search_adv(data, productNameList):
+    for dt in data[0]:
+        priority = 0
+        for pnw in productNameList:
+            if str(pnw.lower()) in str(dt["Product Name"].lower()):
+                priority = priority + 1
+            else:
+                continue
+            prt = {"priority" : priority}
+            dt.update(prt)
+    data = sorted(data[0], key=lambda x: x['priority'], reverse=True)
+
+    if len(data) > 20:
+        data = data[0:20]
+
+    for dt in data:
+        dt.pop("priority")
+    
+    return data
+
 class db:
     mysql = None
 
@@ -50,8 +70,12 @@ class db:
 
                 info = cursor.fetchall()
                 data.append(info)
-                clean_data = [i for n, i in enumerate(data) if i not in data[n + 1:]]
-                return clean_data
+                data = [i for n, i in enumerate(data) if i not in data[n + 1:]]
+
+                data = search_adv(data, productNameList)
+                
+                return data
+
         else:
             query = f"SELECT * FROM `{table}` WHERE 1"
             cursor.execute(query)
