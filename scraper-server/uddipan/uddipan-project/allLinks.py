560eae1f2f428168b473a4ccabe4b2041817f2fc
ParthBarse <parthbarse72@gmail.com>
2022-08-22 00:43:21 +0530

    update

--- scraper-server/uddipan/uddipan-project/allLinks.py
+++ scraper-server/uddipan/uddipan-project/allLinks.py
@@ -1,58 +1,210 @@
-lnk = {}
-
 with open("uniqueLnks_1.txt", "r") as output:
-    lnk["uniqueLnks_1"] = output.read()
+    uniqueLnks_1 = output.read()
 
 with open("uniqueLnks_2.txt", "r") as output:
-    lnk["uniqueLnks_2"] = output.read()
+    uniqueLnks_2 = output.read()
 
 with open("uniqueLnks_3.txt", "r") as output:
-    lnk["uniqueLnks_3"] = output.read()
+    uniqueLnks_3 = output.read()
 
 with open("uniqueLnks_4.txt", "r") as output:
-    lnk["uniqueLnks_4"] = output.read()
+    uniqueLnks_4 = output.read()
 
 with open("uniqueLnks_5.txt", "r") as output:
-    lnk["uniqueLnks_5"] = output.read()
+    uniqueLnks_5 = output.read()
 
 with open("uniqueLnks_6.txt", "r") as output:
-    lnk["uniqueLnks_6"] = output.read()
+    uniqueLnks_6 = output.read()
 
 with open("uniqueLnks_7.txt", "r") as output:
-    lnk["uniqueLnks_7"] = output.read()
+    uniqueLnks_7 = output.read()
 
 with open("uniqueLnks_8.txt", "r") as output:
-    lnk["uniqueLnks_8"] = output.read()
+    uniqueLnks_8 = output.read()
 
 with open("uniqueLnks_9.txt", "r") as output:
-    lnk["uniqueLnks_9"] = output.read()
+    uniqueLnks_9 = output.read()
 
 with open("uniqueLnks_10.txt", "r") as output:
-    lnk["uniqueLnks_10"] = output.read()
+    uniqueLnks_10 = output.read()
 
 with open("uniqueLnks_11.txt", "r") as output:
-    lnk["uniqueLnks_11"] = output.read()
+    uniqueLnks_11 = output.read()
 
 with open("uniqueLnks_12.txt", "r") as output:
-    lnk["uniqueLnks_12"] = output.read()
+    uniqueLnks_12 = output.read()
 
 with open("uniqueLnks_13.txt", "r") as output:
-    lnk["uniqueLnks_13"] = output.read()
+    uniqueLnks_13 = output.read()
 
 with open("uniqueLnks_14.txt", "r") as output:
-    lnk["uniqueLnks_14"] = output.read()
+    uniqueLnks_14 = output.read()
 
 with open("uniqueLnks_15.txt", "r") as output:
-    lnk["uniqueLnks_15"] = output.read()
+    uniqueLnks_15 = output.read()
 
 with open("uniqueLnks_16.txt", "r") as output:
-    lnk["uniqueLnks_16"] = output.read()
+    uniqueLnks_16 = output.read()
 
 with open("uniqueLnks_17.txt", "r") as output:
-    lnk["uniqueLnks_17"] = output.read()
+    uniqueLnks_17 = output.read()
 
 with open("uniqueLnks_18.txt", "r") as output:
-    lnk["uniqueLnks_18"] = output.read()
+    uniqueLnks_18 = output.read()
 
 with open("uniqueLnks_19.txt", "r") as output:
-    lnk["uniqueLnks_19"] = output.read()
+    uniqueLnks_19 = output.read()
+
+ul1 = uniqueLnks_1.split(",")
+ul2 = uniqueLnks_2.split(",")
+ul3 = uniqueLnks_3.split(",")
+ul4 = uniqueLnks_4.split(",")
+ul5 = uniqueLnks_5.split(",")
+ul6 = uniqueLnks_6.split(",")
+ul7 = uniqueLnks_7.split(",")
+ul8 = uniqueLnks_8.split(",")
+ul9 = uniqueLnks_9.split(",")
+ul10 = uniqueLnks_10.split(",")
+ul11 = uniqueLnks_11.split(",")
+ul12 = uniqueLnks_12.split(",")
+ul13 = uniqueLnks_13.split(",")
+ul14 = uniqueLnks_14.split(",")
+ul15 = uniqueLnks_15.split(",")
+ul16 = uniqueLnks_16.split(",")
+ul17 = uniqueLnks_17.split(",")
+ul18 = uniqueLnks_18.split(",")
+ul19 = uniqueLnks_19.split(",")
+
+uniqueLnks_1 = []
+uniqueLnks_2 = []
+uniqueLnks_3 = []
+uniqueLnks_4 = []
+uniqueLnks_5 = []
+uniqueLnks_6 = []
+uniqueLnks_7 = []
+uniqueLnks_8 = []
+uniqueLnks_9 = []
+uniqueLnks_10 = []
+uniqueLnks_11 = []
+uniqueLnks_12 = []
+uniqueLnks_13 = []
+uniqueLnks_14 = []
+uniqueLnks_15 = []
+uniqueLnks_16 = []
+uniqueLnks_17 = []
+uniqueLnks_18 = []
+uniqueLnks_19 = []
+
+for lnk in ul1:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_1.append(slnk)
+
+for lnk in ul2:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_2.append(slnk)
+
+for lnk in ul3:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_3.append(slnk)
+
+for lnk in ul4:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_4.append(slnk)
+
+for lnk in ul5:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_5.append(slnk)
+
+for lnk in ul6:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_6.append(slnk)
+
+for lnk in ul7:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_7.append(slnk)
+
+for lnk in ul8:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_8.append(slnk)
+
+for lnk in ul9:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_9.append(slnk)
+
+for lnk in ul10:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_10.append(slnk)
+
+for lnk in ul11:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_11.append(slnk)
+
+for lnk in ul12:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_12.append(slnk)
+
+for lnk in ul13:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_13.append(slnk)
+
+for lnk in ul14:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_14.append(slnk)
+
+for lnk in ul15:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_15.append(slnk)
+
+for lnk in ul16:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_16.append(slnk)
+
+for lnk in ul17:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_17.append(slnk)
+
+for lnk in ul18:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_18.append(slnk)
+
+for lnk in ul19:
+    slnk = lnk.replace("'", "")
+    slnk = slnk.replace("[", "")
+    slnk = slnk.replace("]", "")
+    uniqueLnks_19.append(slnk)
