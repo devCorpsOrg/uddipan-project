794db818bcad0be891731e8ab2a326db562632df
Parth Barse <parthbarse72@gmail.com>
2023-05-30 19:35:26 +0530

    update

--- scraper-server/uddipan/uddipan-project/main_linux_old.py
+++ scraper-server/uddipan/uddipan-project/main_linux_old.py
@@ -61,8 +61,8 @@ options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 options.add_argument("--disable-dev-shm-usage")
 
-# driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
+# driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
